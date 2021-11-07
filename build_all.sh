3b80a443bf8d337ea6d5a01e30dab79f355ec0c6
Malte kl. Piening <mkl.piening@gmail.com>
2021-11-07 21:22:42 +0100

    added other images to build_all

--- build_all.sh
+++ build_all.sh
@@ -2,19 +2,19 @@
 PRV_KEY="$(cat ~/.ssh/id_rsa)"
 PUB_KEY="$(cat ~/.ssh/id_rsa.pub)"
 
-# echo ""
-# echo "+-------------------------------+"
-# echo "| building ubuntu 20.04 sandbox |"
-# echo "+-------------------------------+"
-# echo ""
-# docker build . --build-arg ssh_prv_key="$PRV_KEY" --build-arg ssh_pub_key="$PUB_KEY" -t ubuntu_20_04_sandbox -f Dockerfile-ubuntu_20_04_sandbox
-# 
-# echo ""
-# echo "+-----------------------------+"
-# echo "| building ros noetic sandbox |"
-# echo "+-----------------------------+"
-# echo ""
-# docker build . --build-arg ssh_prv_key="$PRV_KEY" --build-arg ssh_pub_key="$PUB_KEY" -t ros_noetic_sandbox -f Dockerfile-ros_noetic_sandbox
+echo ""
+echo "+-------------------------------+"
+echo "| building ubuntu 20.04 sandbox |"
+echo "+-------------------------------+"
+echo ""
+docker build . --build-arg ssh_prv_key="$PRV_KEY" --build-arg ssh_pub_key="$PUB_KEY" -t ubuntu_20_04_sandbox -f Dockerfile-ubuntu_20_04_sandbox
+
+echo ""
+echo "+-----------------------------+"
+echo "| building ros noetic sandbox |"
+echo "+-----------------------------+"
+echo ""
+docker build . --build-arg ssh_prv_key="$PRV_KEY" --build-arg ssh_pub_key="$PUB_KEY" -t ros_noetic_sandbox -f Dockerfile-ros_noetic_sandbox
 
 echo ""
 echo "+-------------------------------+"
